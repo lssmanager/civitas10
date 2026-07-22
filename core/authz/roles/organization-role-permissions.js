1fa197e9ae17c860d6f7d86b511cb3ec6c65ea52
lssmanager <manager@learnsocialstudies.com>
2026-07-21 20:44:33 -0500

    fix(authz): add canonical role bundle model

--- core/authz/roles/organization-role-permissions.js
+++ core/authz/roles/organization-role-permissions.js
@@ -1,16 +1,5 @@
 'use strict'
-module.exports = Object.freeze({
-  organization_admin: Object.freeze(['org.documents.create', 'org.documents.read']),
-  organization_director: Object.freeze(['org.documents.read']),
-  organization_headdirector: Object.freeze(['org.documents.read']),
-  organization_headteacher: Object.freeze(['org.documents.read']),
-  organization_groupleader: Object.freeze(['lms.groups.read', 'lms.group_members.read', 'lms.course_offerings.read']),
-  organization_teacher: Object.freeze(['org.documents.read']),
-  organization_student: Object.freeze(['org.documents.read']),
-  organization_parent: Object.freeze(['org.documents.read']),
-  organization_secretary: Object.freeze(['org.documents.read']),
-  organization_accountant: Object.freeze([]),
-  organization_billing: Object.freeze([]),
-  organization_payroll: Object.freeze([]),
-  organization_member: Object.freeze(['org.documents.read']),
-})
+
+const { activeRoleScopes } = require('./generated/role-model')
+
+module.exports = Object.freeze(Object.fromEntries(Object.entries(activeRoleScopes).map(([roleKey, scopes]) => [roleKey, Object.freeze([...scopes].sort())])))
